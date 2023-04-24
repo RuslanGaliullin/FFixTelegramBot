73eb189f2f5cf772dfd87f0d92bc760b768de70a
Vladislav Vasilev <vpvasilev.work@gmail.com>
2023-04-24 14:54:30 +0300

    + сохранение отправленный файлов, отправка и удаление сохраненный файлов

--- bot/main.py
+++ bot/main.py
@@ -1,9 +1,11 @@
+import os
 import telebot
 from telebot import types
 
 bot = telebot.TeleBot("6189522732:AAE6lLTknaQcz008aDRykaiFrkwnE1OJAN8", parse_mode=None)
+split_string = 'SL96illlSfCB5zP'
 
-instruction = ""
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.send_message(message.chat.id, "Добро пожаловать! \nДанный бот создан для того, чтобы упростить "
@@ -14,7 +16,6 @@ def send_welcome(message):
                                       "- Максимальный размер: 100 Мб\n"
                                       "- Минимальная частота дискретизации: 16 кГц\n"
                                       "- Минимальная глубина кодирования: 16 бит\n\n")
-
 
 
 @bot.message_handler(commands=['help'])
@@ -28,24 +29,43 @@ def send_welcome(message):
                                       "<a href='https://t.me/R_Galiullin'>Галлиулин Руслан</a>", parse_mode="HTML")
 
 
-@bot.message_handler(content_types=["audio"])
+@bot.message_handler(content_types=['audio'])
 def message_reply(message):
-    beep = types.InlineKeyboardButton('Запикать',callback_data="b")
-    silence = types.InlineKeyboardButton('Приглушить', callback_data="s")
-    cancel = types.InlineKeyboardButton('Отменить обработку', callback_data="c")
-    buttons=types.InlineKeyboardMarkup([[beep,silence],[cancel]])
+    file_info = bot.get_file(message.audio.file_id)
+    path_to_file = 'recieved_files/' + str(file_info.file_unique_id) + split_string + message.audio.file_name
+    if not path_to_file.split('.')[1] in ['wav', 'mp3']:
+        bot.send_message(message.chat.id, 'Неверный формат файла.')
+        return
 
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(path_to_file, 'wb') as f:
+        f.write(downloaded_file)
+    beep = types.InlineKeyboardButton('Запикать', callback_data='b' + path_to_file)
+    silence = types.InlineKeyboardButton('Приглушить', callback_data='s' + path_to_file)
+    cancel = types.InlineKeyboardButton('Отменить обработку', callback_data='c' + path_to_file)
+    buttons = types.InlineKeyboardMarkup([[beep, silence], [cancel]])
     bot.send_message(message.chat.id, 'Выберите необходимое действие', reply_markup=buttons)
 
 
-@bot.callback_query_handler(func=lambda c: c.data == "s" or c.data == "b")
+@bot.callback_query_handler(func=lambda c: c.data[0] == "s" or c.data[0] == "b")
 def answer_callback_query(callback_query: types.CallbackQuery):
     bot.answer_callback_query(callback_query.id)
+    bot.delete_message(chat_id=callback_query.message.chat.id,
+                       message_id=callback_query.message.message_id)
     bot.send_message(callback_query.from_user.id, 'Выполняется обработка файла')
+    with open(callback_query.data[1:], 'rb') as file:
+        f = file.read()
+    bot.send_document(callback_query.from_user.id, f, visible_file_name=callback_query.data[1:].split(split_string)[1])
+    os.remove(callback_query.data[1:])
 
-@bot.callback_query_handler(func=lambda c: c.data =="c")
+
+@bot.callback_query_handler(func=lambda c: c.data[0] == "c")
 def answer_callback_query(callback_query: types.CallbackQuery):
     bot.answer_callback_query(callback_query.id)
+    bot.delete_message(chat_id=callback_query.message.chat.id,
+                       message_id=callback_query.message.message_id)
     bot.send_message(callback_query.from_user.id, 'Обработка отменена')
+    os.remove(callback_query.data[1:])
+
 
 bot.infinity_polling(none_stop=True)
