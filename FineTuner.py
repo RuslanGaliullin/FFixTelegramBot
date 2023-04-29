18637a0b1295ae5cf2e0eca6b563ae47ea2b4123
RuslanGaliullin <rmgaliullin@edu.hse.ru>
2023-04-29 16:04:18 +0300

    changed gitignore a bit and Finetuner is in correct state now

--- FineTuner.py
+++ FineTuner.py
@@ -14,6 +14,7 @@ transcript_data_dir = os.path.join(os.getcwd(), "data", "transcript")
 # if you pass a new token set for an already finetuned model, it'll be ignored during training
 
 # define your train/eval data
+
 train_data = []
 
 for path in os.listdir(transcript_data_dir):
@@ -27,13 +28,12 @@ model_to_train.finetune(
     output_dir,
     num_workers=3,
     train_data=train_data,
-    eval_data=[random.choice(train_data)],  # the eval_data
+    eval_data=[random.choice(train_data) for i in range(10)],  # the eval_data
     token_set=model_to_train.token_set,
     training_args=TrainingArguments(overwrite_output_dir=True)
 )
-print(model_to_train.transcribe(["/home/user/FFixTelegramBot/data/audio/audio_25.wav"])[0]["transcription"])
-
-model_to_train.processor.push_to_hub("rmgaliullin/wav2vec2-based-obscenity-detector",
-                                     use_auth_token="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")
+# print(model_to_train.transcribe(["/home/user/FFixTelegramBot/data/audio/audio_25.wav"])[0]["transcription"])
+# model_to_train.trainer.save_model("rmgaliullin/wav2vec2-based-obscenity-detector",
+#                                   use_auth_token="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")
 model_to_train.model.push_to_hub("rmgaliullin/wav2vec2-based-obscenity-detector",
                                  use_auth_token="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")
