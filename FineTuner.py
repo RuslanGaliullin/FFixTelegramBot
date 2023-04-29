57c2d5b5a1144e5c2e3f7b947318a0d3e7310eaa
RuslanGaliullin <rmgaliullin@edu.hse.ru>
2023-04-29 15:35:42 +0300

    added some less vulgar words like капец and expanded obsenity words vocab

--- FineTuner.py
+++ FineTuner.py
@@ -1,10 +1,11 @@
 import random
 
 from huggingsound import SpeechRecognitionModel, TrainingArguments
+
 from transformers import AutoModelForCTC, Wav2Vec2Processor
 import os
 
-model = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-russian", device="cuda")
+model_to_train = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-russian", device="cpu")
 output_dir = os.path.join(os.getcwd(), "model_settings", "model_checkpoints")
 audio_data_dir = os.path.join(os.getcwd(), "data", "audio")
 transcript_data_dir = os.path.join(os.getcwd(), "data", "transcript")
@@ -18,20 +19,21 @@ train_data = []
 for path in os.listdir(transcript_data_dir):
     text = open(os.path.join(transcript_data_dir, path)).read()
     audio_path = path.replace("transcript", "audio").replace("txt", "wav")
-    if path != "transcript_18.txt" and path != "transcript_20.txt":
-        train_data.append(
-            {"path": os.path.join(audio_data_dir, audio_path), "transcription": text})
-print(train_data)
+    train_data.append(
+        {"path": os.path.join(audio_data_dir, audio_path), "transcription": text})
+
 # and finally, fine-tune your model
-model.finetune(
+model_to_train.finetune(
     output_dir,
     num_workers=3,
     train_data=train_data,
     eval_data=[random.choice(train_data)],  # the eval_data
-    token_set=model.token_set,
+    token_set=model_to_train.token_set,
     training_args=TrainingArguments(overwrite_output_dir=True)
 )
-print(model.transcribe(["/home/user/FFixTelegramBot/data/audio/audio_25.wav"])[0]["transcription"])
+print(model_to_train.transcribe(["/home/user/FFixTelegramBot/data/audio/audio_25.wav"])[0]["transcription"])
 
-model.processor.push_to_hub("rmgaliullin/wav2vec2-based-obscenity-detector",
-                            use_auth_token="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")
+model_to_train.processor.push_to_hub("rmgaliullin/wav2vec2-based-obscenity-detector",
+                                     use_auth_token="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")
+model_to_train.model.push_to_hub("rmgaliullin/wav2vec2-based-obscenity-detector",
+                                 use_auth_token="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")
