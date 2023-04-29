54b133cfda95a42eb88882455b5ceccbcbd0c69f
RuslanGaliullin <rmgaliullin@edu.hse.ru>
2023-04-29 17:07:45 +0300

    add train result files

--- FineTuner.py
+++ FineTuner.py
@@ -1,7 +1,7 @@
 import random
 
 from huggingsound import SpeechRecognitionModel, TrainingArguments
-
+from sklearn.model_selection import train_test_split
 from transformers import AutoModelForCTC, Wav2Vec2Processor
 import os
 
@@ -9,31 +9,32 @@ model_to_train = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-r
 output_dir = os.path.join(os.getcwd(), "model_settings", "model_checkpoints")
 audio_data_dir = os.path.join(os.getcwd(), "data", "audio")
 transcript_data_dir = os.path.join(os.getcwd(), "data", "transcript")
-# first of all, you need to define your model's token set
-# however, the token set is only needed for non-finetuned models
-# if you pass a new token set for an already finetuned model, it'll be ignored during training
 
-# define your train/eval data
+data = []
 
-train_data = []
-
+# Writing paths to audio and its transcription to train
 for path in os.listdir(transcript_data_dir):
-    text = open(os.path.join(transcript_data_dir, path)).read()
+    text = open(os.path.join(transcript_data_dir, path)).read().strip()
     audio_path = path.replace("transcript", "audio").replace("txt", "wav")
-    train_data.append(
+    data.append(
         {"path": os.path.join(audio_data_dir, audio_path), "transcription": text})
 
-# and finally, fine-tune your model
-model_to_train.finetune(
-    output_dir,
-    num_workers=3,
-    train_data=train_data,
-    eval_data=[random.choice(train_data) for i in range(10)],  # the eval_data
-    token_set=model_to_train.token_set,
-    training_args=TrainingArguments(overwrite_output_dir=True)
-)
-# print(model_to_train.transcribe(["/home/user/FFixTelegramBot/data/audio/audio_25.wav"])[0]["transcription"])
-# model_to_train.trainer.save_model("rmgaliullin/wav2vec2-based-obscenity-detector",
-#                                   use_auth_token="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")
-model_to_train.model.push_to_hub("rmgaliullin/wav2vec2-based-obscenity-detector",
-                                 use_auth_token="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")
+
+X_train, X_test = train_test_split(data, test_size=0.33)
+
+def run_finetune(model):
+    model.finetune(
+        output_dir,
+        num_workers=3,
+        train_data=X_train,
+        eval_data=X_test,  # the eval_data
+        token_set=model.token_set,
+        training_args=TrainingArguments(overwrite_output_dir=True)
+    )
+
+
+# Saving
+# model_to_train.model.push_to_hub("rmgaliullin/wav2vec2-based-obscenity-detector",
+#                                  use_auth_token="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")
+
+print(model_to_train.evaluate([X_test[0]]))
