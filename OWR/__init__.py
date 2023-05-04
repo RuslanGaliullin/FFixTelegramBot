872696fa33e8e82c37964c89933b5109d067f425
Ruslan Galiullin <rusgal000@gmail.com>
2023-05-04 12:09:44 +0300

    add evaluation function

--- OWR/__init__.py
+++ OWR/__init__.py
@@ -11,6 +11,7 @@ from playsound import playsound
 import pylcs
 import logging
 import inspect
+from huggingsound.metrics import cer, wer
 
 # Setup logging
 logger = logging.getLogger(__name__)
@@ -26,28 +27,26 @@ class ObscenityWordsRecognizer:
     soundex = RussianSoundex(delete_first_letter=True, reduce_word=True)
 
     @staticmethod
-    def __get_phonetic_for_words(path_to_words: str) -> tuple[dict[str, str], dict[str, str]]:
+    def __get_phonetic_for_words(path_to_words: str) -> dict[str, str]:
         soundex = RussianSoundex(delete_first_letter=True)
         result_word_to_code = {}
-        result_code_to_word = {}
         with open(path_to_words, 'rb') as fp:
             words = json.load(fp)
         for i in words:
-            result_code_to_word[soundex.transform(i)] = i
             result_word_to_code[i] = soundex.transform(i)
-        return result_code_to_word, result_word_to_code
+        return result_word_to_code
 
     @staticmethod
     def __cover_by_sound(samples: np.ndarray[np.float32], words_to_delete: list[tuple], mode: str) -> None:
         if mode == 's':
             for i in words_to_delete:
-                if (samples.ndim == 1):
+                if samples.ndim == 1:
                     samples[i[0]:i[1]] = np.zeros(i[1] - i[0])
                 else:
                     samples[i[0]:i[1]] = np.zeros((i[1] - i[0], samples.ndim))
         if mode == 'b':
             for i in words_to_delete:
-                if (samples.ndim == 1):
+                if samples.ndim == 1:
                     samples[i[0]:i[1]] = librosa.tone(600, length=i[1] - i[0], sr=16000)
                 else:
                     samples[i[0]:i[1]] = np.tile((librosa.tone(600, length=i[1] - i[0], sr=16000)),
@@ -69,7 +68,7 @@ class ObscenityWordsRecognizer:
 
         # y, sr = librosa.load(resampled_audio, mono=True, sr=16000)
 
-        words_to_delete = self.__find_words(self.ASR.transcribe([resampled_audio]), origin_sr)
+        words_to_delete = self.__find_words(self.ASR.transcribe([resampled_audio])[0], origin_sr)
         self.__cover_by_sound(origin_samples, words_to_delete, mode)
 
         directory = os.path.dirname(audio_path)
@@ -96,7 +95,7 @@ class ObscenityWordsRecognizer:
         res_ow = ''
         word_t = ObscenityWordsRecognizer.soundex.transform(word)
         s_len = int(len(word_t) / 3)
-        for ow in self.__phonetic_word_codes[1].keys():
+        for ow in self.__phonetic_word_codes.keys():
             if word_t[0:s_len] == ObscenityWordsRecognizer.soundex.transform(ow)[0:s_len]:
                 indexes = pylcs.lcs_sequence_idx(word, ow)
                 probability = 0
@@ -105,18 +104,18 @@ class ObscenityWordsRecognizer:
                         probability += probabilities[index]
                 dist = Levenshtein.distance(ow, word)
                 probability = probability / max(len(word), len(ow))
-                if ((probability / (dist + 1) ** 0.25) ** dist >= result):
+                if (probability / (dist + 1) ** 0.25) ** dist >= result:
                     result = (probability / (dist + 1) ** 0.25) ** dist
                     res_ow = ow
-        return (int(result * 10) / 10, res_ow)
+        return int(result * 10) / 10, res_ow
 
-    def __find_words(self, transcribe_result: list[dir], sample_rate: int) -> list[tuple]:
+    def __find_words(self, transcribe_result: dir, sample_rate: int, evaluation: list[str] = None) -> list[tuple]:
         o_words = []
-        sentence = transcribe_result[0]['transcription']
+        sentence = transcribe_result['transcription']
         logger.info(f"Transcription: {sentence}")
-        probabilities = transcribe_result[0]['probabilities']
-        start_timestamps = transcribe_result[0]["start_timestamps"]
-        end_timestamps = transcribe_result[0]["end_timestamps"]
+        probabilities = transcribe_result['probabilities']
+        start_timestamps = transcribe_result["start_timestamps"]
+        end_timestamps = transcribe_result["end_timestamps"]
 
         for length in range(2, 8):
             for i in range(0, len(sentence) - length + 1):
@@ -153,8 +152,32 @@ class ObscenityWordsRecognizer:
                     end = end_timestamps[-1] * (sample_rate // 1000)
                 ow_timestamps.append((start, end))
         # ows - all recognised words
-        print(ows)
+        if evaluation:
+            evaluation.extend(ows)
         return ow_timestamps
+
+    def evaluate(self, audio_transcript_data):
+        data = []
+        cer_ow = 0
+        wer_ow = 0
+        true_positive = 0
+        total = 0
+        for audio_path, transcript_path in audio_transcript_data:
+            prediction_ow = []
+            text = open(transcript_path).read().strip()
+            data.append({'path': audio_path, 'transcription': text})
+            reference_ow = [i for i in self.__phonetic_word_codes.keys() if i in text]
+            self.__find_words(self.ASR.transcribe([audio_path])[0], 16000, prediction_ow)
+            true_positive += len(prediction_ow)
+            total += len(reference_ow)
+            cer_ow = cer(prediction_ow, reference_ow)
+            wer_ow = wer(prediction_ow, reference_ow)
+
+        return {"total cer": self.ASR.evaluate(data)["cer"],
+                "total wer": self.ASR.evaluate(data)["wer"],
+                "obscenity cer": cer_ow,
+                "obscenity wer": wer_ow,
+                "rate of word detection": true_positive / total}
 
 
 if __name__ == "__main__":
