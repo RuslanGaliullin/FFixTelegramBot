4499e4ebb2e7b116c0f53c9a01d9ed9d943eaf36
Ruslan Galiullin <rusgal000@gmail.com>
2023-05-06 23:32:00 +0300

    done args parsing

--- FineTuner.py
+++ FineTuner.py
@@ -1,13 +1,12 @@
 from huggingsound import SpeechRecognitionModel, TrainingArguments
 from sklearn.model_selection import train_test_split
 import os
+import argparse, sys
 
 
-def fine_tune():
-    model_to_train = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-russian", device="cpu")
-    output_dir = os.path.join(os.getcwd(), "model_settings", "model_checkpoints")
-    audio_data_dir = os.path.join(os.getcwd(), "data", "audio")
-    transcript_data_dir = os.path.join(os.getcwd(), "data", "transcript")
+def load_data(path_to_audio: str, path_to_transcript: str):
+    audio_data_dir = path_to_audio
+    transcript_data_dir = path_to_transcript
 
     data = []
 
@@ -17,26 +16,46 @@ def fine_tune():
         audio_path = path.replace("transcript", "audio").replace("txt", "wav")
         data.append(
             {"path": os.path.join(audio_data_dir, audio_path), "transcription": text})
+    print(data)
 
-    X_train, X_test = train_test_split(data, test_size=0.33)
+    return data
+
+
+def fine_tune(train_data: list[dir], device: str = 'cpu'):
+    model_to_train = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-russian", device=device)
+    output_dir = os.path.join(os.getcwd(), "model_settings", "model_checkpoints")
 
     model_to_train.finetune(
         output_dir,
         num_workers=3,
-        train_data=X_train,
-        eval_data=X_test,  # the eval_data
+        train_data=train_data,
         token_set=model_to_train.token_set,
         training_args=TrainingArguments(overwrite_output_dir=True)
     )
     return model_to_train
 
 
-# Saving
-# model_to_train.model.push_to_hub("rmgaliullin/wav2vec2-based-obscenity-detector",
-#                                  use_auth_token="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")
-
 if __name__ == "__main__":
-    result_model = fine_tune()
-    # result_model.model.push_to_hub("rmgaliullin/wav2vec2-based-obscenity-detector",
-    #                                use_auth_token="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")
-    result_model.evaluate()
+    if len(sys.argv) != 7:
+        print(
+            "python FineTuner.py --audio --transcript --percent --evaluation --device --push")
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--audio", help="path to dir with audio files")
+    parser.add_argument("--transcript", help="path to dir with transcripts")
+    parser.add_argument("--percent", help="float value from (0; 1) proportion of data will be used for test")
+    parser.add_argument("--evaluation", help="flag 0 or 1 to evaluate result model or not")
+    parser.add_argument("--device", help="cpu or cuda device to compute")
+    parser.add_argument("--push", help="flag 0 or 1 save weights in hugging face hub or not")
+    args = parser.parse_args()
+    if args.evaluation == '0':
+        X_train, X_test = load_data(args.audio, args.transcript)
+    else:
+        X_train, X_test = train_test_split(load_data(args.audio, args.transcript), test_size=float(args.percent))
+
+    result_model = fine_tune(X_train, device=args.device)
+    if args.push == '1':
+        result_model.model.push_to_hub("rmgaliullin/wav2vec2-based-obscenity-detector",
+                                       use_auth_token="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")
+    if args.evaluation == '1':
+        print(result_model.evaluate(X_test))
